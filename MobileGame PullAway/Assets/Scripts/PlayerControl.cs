b7454721ea99e7ead4315fee4c05731146e0f8fc
firefoxfenix <christiancarriespi@Gmail.com>
2015-07-14 20:24:41 -0500

    Touch Input
    Drag , seleccion de objetos y posición en la pantalla

--- MobileGame PullAway/Assets/Scripts/PlayerControl.cs
+++ MobileGame PullAway/Assets/Scripts/PlayerControl.cs
@@ -3,41 +3,81 @@ using System.Collections;
 
 public class PlayerControl : MonoBehaviour {
 
-	public float Speed = Screen.height;
-	private Touch touch;
-	private int fingerCount=0;
-	private Vector3 cambioV= new Vector3(), actual= new Vector3(), cambioH= new Vector3();
-	public GameObject player=null;
+
+	Touch touch;
+	int fingerCount=0;
+	private Ray pulsacion;
+	private RaycastHit colision;
+	Vector3 cambioV= new Vector3(), actual= new Vector3(), cambioH= new Vector3();
+	private GameObject player=null;
+	bool bd=false;
+	Vector3 Pos;
 	// Use this for initialization
 	void Start () 
 	{
-		Speed = Screen.height;
+
 	}
 
 
 	// Update is called once per frame
 	void Update () {
-		try{
 
-		if (Input.GetTouch(0).phase == TouchPhase.Ended) {
-			cambioV.Set (0, Input.GetTouch (0).position.y / (Screen.height*5f), 0);
-			cambioH.Set (Input.GetTouch (0).position.x / Screen.width, 0, 0);
-			}
-			// Los valores de 0.1 , 0.2 son para que se mueva adelante-atras-arriba 
-			//o abajo dependiendo de un {cuadrante jaja} 
-			if(Input.GetTouch (0).position.y/ Screen.height >0.2f)
-				player.transform.Translate(Vector3.up * Speed * Time.deltaTime);
-			if(Input.GetTouch (0).position.y/ Screen.height <0.2f)
-				player.transform.Translate(Vector3.down * Speed * Time.deltaTime);
-			if(Input.GetTouch (0).position.x/ Screen.width >0.2f)
-				player.transform.position+= cambioH;
-			if(Input.GetTouch (0).position.x/ Screen.width <0.2f)
-				player.transform.position-= cambioH;
-		
-		}catch(System.Exception)
+
+		if(Input.GetTouch(0).phase== TouchPhase.Began)
 		{
+			//Cambia la posicion del touch por la posicion en la pantalla, es como un render
+			Pos = Camera.main.ScreenToWorldPoint(Input.GetTouch(0).position);
+			//Usamos un raycast 2d para hallar el collider
+			RaycastHit2D hit = Physics2D.Raycast(new Vector2(Pos.x, Pos.y), Vector2.zero);
+
+			if(hit != null && hit.collider != null){
+				bd = true;
+				//designamos el objeto a mover dependiendo del collider tocado
+				player=GameObject.Find(hit.collider.gameObject.name);
+
+			}
+
 		}
-		 
+
+		//Esta es una alternativa de movimiento usando la posicion renderizada, aunque no me funciono mucho la dejo ahi
+		//por si les sirva de algo
+		/*Pos = Camera.main.ScreenToWorldPoint(Input.GetTouch(0).position);
+		if ((Input.GetTouch (0).phase == TouchPhase.Ended) && (bd)) 
+		{
+
+			cambioV.Set (Pos.y,0 , 0);
+			cambioH.Set (Pos.x, 0, 0);
+			bd=false;
+		}
+		if (Pos.y > (Screen.height/2))
+			player.transform.position += cambioV;
+		if (Pos.y < (Screen.height/2))
+			player.transform.position -= cambioV;
+		if (Pos.x > (Screen.width/2))
+			player.transform.position += cambioH;
+		if (Pos.x < (Screen.width/2))
+			player.transform.position -= cambioH;
+		*/
+
+		//Movemos el objeto
+		if ((Input.GetTouch (0).phase == TouchPhase.Ended)&&(bd)) 
+		{
+			cambioV.Set (0, Input.GetTouch (0).position.y / (Screen.height * 5f), 0);
+			cambioH.Set (Input.GetTouch (0).position.x / Screen.width, 0, 0);
+			bd=false;
+		}
+		// Los valores de 0.1 , 0.2 son para que se mueva adelante-atras-arriba 
+		//o abajo dependiendo de un {cuadrante jaja} 
+		if (Input.GetTouch (0).position.y / Screen.height > 0.2f)
+			player.transform.position += cambioV;
+		if (Input.GetTouch (0).position.y / Screen.height < 0.2f)
+			player.transform.position -= cambioV;
+		if (Input.GetTouch (0).position.x / Screen.width > 0.2f)
+			player.transform.position += cambioH;
+		if (Input.GetTouch (0).position.x / Screen.width < 0.2f)
+			player.transform.position -= cambioH;
+
+		
 
 		//Esta parte activa y desactiva el objeto dependiendo de cuantos dedos tocan la pantalla
 		/*if (Input.touchCount > 2)
@@ -51,7 +91,5 @@ public class PlayerControl : MonoBehaviour {
 
 
 		}*/
-
-	
 	}
 }
