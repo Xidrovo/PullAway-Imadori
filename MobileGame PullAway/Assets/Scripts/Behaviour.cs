29fc7bf3b7060225244b2edf70d721dacf150a56
Xidrovo <Xidrovo@hotmail.com>
2015-07-16 21:47:35 -0400

    :accept:
    :+1: :+1: :+1:

--- MobileGame PullAway/Assets/Scripts/Behaviour.cs
+++ MobileGame PullAway/Assets/Scripts/Behaviour.cs
@@ -4,37 +4,46 @@ using System.Collections;
 public class Behaviour : MonoBehaviour {
 
 	// Use this for initialization
-	private int i = 0;
-	private Vector3 radio;
+	private int theta = 0;
+	private Vector3 radio, Pos;
+	public GameObject Temporito;
+	private float Angle;
 	void Start () 
 	{
-		radio=this.transform.position;
-	}
-	
-	// Update is called once per frame
-	void Update () 
-	{
-
-	
+		Pos = this.transform.position;
 	}
 
-	public void OnTriggerEnter2D ()
+	void Update()
 	{
+		this.transform.position = new Vector3 (Pos.x, this.transform.position.y, 0);
+	}
 
-		if (i >= 360) 
+	void OnTriggerEnter2D (Collider2D trigg)
+	{
+		if (theta >= 360) {
+			Pos.x = this.transform.position.x + 1;
+			theta = 0;
+		} else
 		{
-			this.transform.position+=(new Vector3(3,3,0));
-			i=0;
+			Angle = (theta * Mathf.PI)/180;
+			this.transform.position = new Vector3 (Mathf.Cos(Angle),  Mathf.Sin(Angle),0) * 2 ;
+			theta ++;
+		}
+
+
+/*		if (i >= 360) 
+		{
+			this.transform.position += (new Vector3((2 * Screen.width/50),0,0));
+			i = 0;
 		} 
 		else 
 		{
 			//float angle = i * Mathf.PI * 2/ number Off layer that the object is on ;
 			float angle =i;
-			Vector3 pos = new Vector3(Mathf.Cos(angle),  Mathf.Sin(angle),0) * radio.magnitude;
+
 			this.transform.position = pos;
 			i++;
 		}
-		Destroy (this);
-
+*/
 	}
 }
