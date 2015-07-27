299ade3e1b0a27eb985e45d5780e0b3caa10b524
firefoxfenix <christiancarriespi@Gmail.com>
2015-07-26 22:40:59 -0500

    Start menu

---
+++ MobileGame PullAway/Assets/Scripts/Continuar.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class Continuar : MonoBehaviour {
+
+	// Use this for initialization
+	public void LoadLevel(string levelName) 
+	{
+		Application.LoadLevel ("MainGame");
+	
+	}
+	
+	// Update is called once per frame
+	public void ExitApplication () 
+	{
+		Application.Quit ();
+	
+	}
+}
