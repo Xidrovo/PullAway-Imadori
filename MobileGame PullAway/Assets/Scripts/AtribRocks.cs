145f5c32c6a9ff913b6e51b6c70e5d9e62a04571
firefoxfenix <christiancarriespi@Gmail.com>
2015-08-15 11:10:30 -0500

    atributos rocas

---
+++ MobileGame PullAway/Assets/Scripts/AtribRocks.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class AtribRocks : MonoBehaviour {
+
+	// Use this for initialization
+	public int life, layer, score;
+	public static bool duty;
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+		if (duty) 
+		{
+			this.layer++;
+
+		}
+	}
+
+}
--- MobileGame PullAway/Assets/Scripts/Eliminar.cs
+++ MobileGame PullAway/Assets/Scripts/Eliminar.cs
@@ -4,30 +4,31 @@ using System.Collections;
 public class Eliminar : MonoBehaviour {
 
 	public CreateRamdomly  random;
+	private AtribRocks atb;
 
 	// Use this for initialization
-	void Start () {
-	
+	void Start () 
+	{
+		atb=this.GetComponent<AtribRocks>();
 	}
 	
 	// Update is called once per frame
 	void Update () {
+
 		try
 		{
 			if((this.transform.position.y>=16f)||(this.transform.position.y<=-16f))
 			{
-				Debug.Log("Kaboom!!");
+
+
 				Destroy(this.gameObject);
 				//Incrementar el Layer a toooodas las rocas
-
 				random.CreateObject(1);
 			}
 			if((this.transform.position.x<=-25f)||(this.transform.position.x>=25f))
 			{
-				Debug.Log("Kaboom!!");
 				Destroy(this.gameObject);
 				//Incrementar el Layer a toooodas las rocas
-				
 				random.CreateObject(1);
 			}
 
