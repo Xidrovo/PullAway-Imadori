83c16de9ec2a154e59b9f4c7943acc40a090ff93
Xidrovo <Xidrovo@hotmail.com>
2015-09-12 12:27:19 -0500

    :dancers:
    :3

--- MobileGame PullAway/Assets/Scripts/AtribRocks.cs
+++ MobileGame PullAway/Assets/Scripts/AtribRocks.cs
@@ -5,17 +5,29 @@ public class AtribRocks : MonoBehaviour {
 
 	// Use this for initialization
 	public int life, layer, score;
-	private SpriteRenderer Sr;
+	private SpriteRenderer Sr, Temp;
+    private float Resultado;
+    private Collider2D coll;
 
 	void Start () {
 		Sr = this.GetComponent<SpriteRenderer> ();
-		this.layer = Sr.sortingOrder; 
+        Temp = Sr;
+		this.layer = Sr.sortingOrder;
+        coll = this.gameObject.GetComponent<Collider2D>();
+        Resultado = ((float)(30 - this.layer) / 100);
+        Sr.color = new Color(Temp.color.r - Resultado, Temp.color.g - Resultado, Temp.color.b - Resultado, Temp.color.a - Resultado);
 	}
 	
 	// Update is called once per frame
 	void Update () 
 	{
-
+        if(this.layer == 30)
+        {
+            coll.enabled = true;
+        }else
+        {
+            coll.enabled = false;
+        }
 	}
 
 	public void SortingUp()
@@ -24,7 +36,9 @@ public class AtribRocks : MonoBehaviour {
 			Sr.sortingOrder = Sr.sortingOrder + 1;
 			this.layer = Sr.sortingOrder;
 			this.gameObject.name = "Roquita" + "" + Sr.sortingOrder;
-		}
+            Resultado = ((float)(30 - this.layer) / 100);
+            Sr.color = new Color(Temp.color.r - Resultado, Temp.color.g - Resultado, Temp.color.b - Resultado, Temp.color.a - Resultado);
+        }
 		catch(System.Exception e)
 		{
 		}
