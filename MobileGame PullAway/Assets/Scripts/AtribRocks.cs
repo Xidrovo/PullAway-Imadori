bbc2b92cd9d1b051c8d23af3c1774f22854b979c
firefoxfenix <christiancarriespi@Gmail.com>
2015-09-12 22:38:09 -0500

    Revert ":dancers:"
    This reverts commit 83c16de9ec2a154e59b9f4c7943acc40a090ff93.
    
    Conflicts:
    	MobileGame PullAway/Assets/Scripts/AtribRocks.cs
    	MobileGame PullAway/Library/CurrentLayout.dwlt
    	MobileGame PullAway/Library/ProjectSettings.asset
    	MobileGame PullAway/Library/ScriptAssemblies/Assembly-CSharp.dll
    	MobileGame PullAway/Library/ScriptAssemblies/Assembly-CSharp.dll.mdb
    	MobileGame PullAway/Library/ScriptAssemblies/BuiltinAssemblies.stamp
    	MobileGame PullAway/Library/ScriptMapper
    	MobileGame PullAway/Library/assetDatabase3
    	MobileGame PullAway/Library/expandedItems
    	MobileGame PullAway/Library/metadata/00/00000000000000002000000000000000
    	MobileGame PullAway/Library/metadata/00/00000000000000003000000000000000
    	MobileGame PullAway/Library/metadata/00/00000000000000004000000000000000
    	MobileGame PullAway/Library/metadata/00/00000000000000004100000000000000
    	MobileGame PullAway/Library/metadata/00/00000000000000005000000000000000
    	MobileGame PullAway/Library/metadata/00/00000000000000005100000000000000
    	MobileGame PullAway/Library/metadata/00/00000000000000006000000000000000
    	MobileGame PullAway/Library/metadata/00/00000000000000006100000000000000
    	MobileGame PullAway/Library/metadata/00/00000000000000007000000000000000
    	MobileGame PullAway/Library/metadata/00/00000000000000008000000000000000
    	MobileGame PullAway/Library/metadata/00/00000000000000009000000000000000
    	MobileGame PullAway/Library/metadata/00/0000000000000000a000000000000000
    	MobileGame PullAway/Library/metadata/00/0000000000000000b000000000000000
    	MobileGame PullAway/Library/metadata/00/0000000000000000c000000000000000
    	MobileGame PullAway/Library/metadata/1a/1a5e3e2025218e141a38f107c6cb7e9c
    	MobileGame PullAway/Library/metadata/26/26dfaa0d2230231458210fc617734a11
    	MobileGame PullAway/Library/metadata/4c/4c023fbc8635d1247a6374753c252dd8
    	MobileGame PullAway/Library/metadata/5f/5f32cd94baa94578a686d4b9d6b660f7
    	MobileGame PullAway/Library/metadata/71/712f1ff8bf97fb447a990a3ae7575a8a
    	MobileGame PullAway/Library/metadata/80/80a3616ca19596e4da0f10f14d241e9f
    	MobileGame PullAway/Library/metadata/87/870353891bb340e2b2a9c8707e7419ba
    	MobileGame PullAway/Library/metadata/8f/8fa9f977cbd637f48b4e5b7ffba934ef
    	MobileGame PullAway/Library/metadata/94/94b24641e7af3ba40b08e355b771907b
    	MobileGame PullAway/Library/metadata/f5/f5f67c52d1564df4a8936ccd202a3bd8
    	MobileGame PullAway/Library/metadata/f6/f66a6d59ed2f400439ef7d7e951e2ae9
    	MobileGame PullAway/Library/shadercompiler-32bit-1.log
    	MobileGame PullAway/MobileGame PullAway.sln
    	MobileGame PullAway/ProjectSettings/ProjectSettings.asset

--- MobileGame PullAway/Assets/Scripts/AtribRocks.cs
+++ MobileGame PullAway/Assets/Scripts/AtribRocks.cs
@@ -5,29 +5,17 @@ public class AtribRocks : MonoBehaviour {
 
 	// Use this for initialization
 	public int life, layer, score;
-	private SpriteRenderer Sr, Temp;
-    private float Resultado;
-    private Collider2D coll;
+	private SpriteRenderer Sr;
 
 	void Start () {
 		Sr = this.GetComponent<SpriteRenderer> ();
-        Temp = Sr;
-		this.layer = Sr.sortingOrder;
-        coll = this.gameObject.GetComponent<Collider2D>();
-        Resultado = ((float)(30 - this.layer) / 100);
-        Sr.color = new Color(Temp.color.r - Resultado, Temp.color.g - Resultado, Temp.color.b - Resultado, Temp.color.a - Resultado);
+		this.layer = Sr.sortingOrder; 
 	}
 	
 	// Update is called once per frame
 	void Update () 
 	{
-        if(this.layer == 30)
-        {
-            coll.enabled = true;
-        }else
-        {
-            coll.enabled = false;
-        }
+
 	}
 
 	public void SortingUp()
@@ -36,9 +24,13 @@ public class AtribRocks : MonoBehaviour {
 			Sr.sortingOrder = Sr.sortingOrder + 1;
 			this.layer = Sr.sortingOrder;
 			this.gameObject.name = "Roquita" + "" + Sr.sortingOrder;
+<<<<<<< HEAD
 			Sr.color = new Color(Temp.color.r - Resultado, Temp.color.g - Resultado, Temp.color.b - Resultado, Temp.color.a - Resultado);
 			Resultado = ((float)(30 - this.layer) / 100);
         }
+=======
+		}
+>>>>>>> parent of 83c16de... :dancers:
 		catch(System.Exception e)
 		{
 		}
