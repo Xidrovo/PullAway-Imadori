966452814c2f66ae01f39340fde04e0a2dbf48cd
firefoxfenix <christiancarriespi@Gmail.com>
2015-09-15 23:27:22 -0500

    cambios
    botones menus, logros

---
+++ MobileGame PullAway/Assets/Scripts/Rocks/Eliminar.cs
@@ -0,0 +1,113 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Eliminar : MonoBehaviour {
+
+	public CreateRamdomly  random;
+	private AtribRocks atb;
+    private AudioSource fxSound;
+	private int Tipo;
+	public static bool Duty = false;
+	public static int ReloadLayout;
+
+	// Use this for initialization
+	void Start () 
+	{
+		atb = this.GetComponent<AtribRocks>();
+		ReloadLayout = random.Max;
+        fxSound=this.GetComponent<AudioSource>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		try
+		{
+            int rnd = Random.Range(1,4);
+			if((this.transform.position.y>=5f)||(this.transform.position.y<=-5f))
+			{
+                fxSound.Play();
+				Duty = true;
+				CreateRamdomly.AllRocks.Remove(this.gameObject);
+				Bida.LifeUp();
+                GeneralAttrib.metros++;
+                GeneralAttrib.Damage += 0.005f;
+                Traps.death++;
+                if (GeneralAttrib.metros != 0 && GeneralAttrib.metros % 100 == 0)
+                {
+                    GeneralAttrib.Damage *= 1.4f;
+                }
+				Destroy(this.gameObject);
+                if (GeneralAttrib.metros != 0 )
+                {
+                    if (GeneralAttrib.metros == 20)
+                    {
+                        
+                        achivements.Generar();
+                    }
+                    if (GeneralAttrib.metros % 50 == 0)
+                    {
+                        
+                        achivements.Generar(rnd);
+                    }
+                    else if (GeneralAttrib.metros % 75 == 0)
+                    {
+                        
+                        achivements.Generar(rnd);
+                    }
+                    else if (GeneralAttrib.metros % 100 == 0)
+                    {
+                     
+                        achivements.Generar(rnd);
+                    }
+                }
+			}
+			if((this.transform.position.x<=-14f)||(this.transform.position.x>=14f))
+			{
+                fxSound.Play();
+				Duty = true;
+				CreateRamdomly.AllRocks.Remove(this.gameObject);
+				Bida.LifeUp();
+                GeneralAttrib.metros++;
+                GeneralAttrib.Damage += 0.0005f;
+                Traps.death++;
+                if (GeneralAttrib.metros != 0 && GeneralAttrib.metros % 100 == 0)
+                {
+                    GeneralAttrib.Damage *= 1.4f;
+                }
+				Destroy(this.gameObject);
+                if (GeneralAttrib.metros != 0)
+                {
+                    if (GeneralAttrib.metros == 20)
+                    {
+                        achivements.Generar();
+                    }
+                    if (GeneralAttrib.metros % 50 == 0)
+                    {
+                        achivements.Generar(rnd);
+                    }
+                    else if (GeneralAttrib.metros % 75 == 0)
+                    {
+                        achivements.Generar(rnd);
+                    }
+                    else if (GeneralAttrib.metros % 100 == 0)
+                    {
+                        achivements.Generar(rnd);
+                    }
+                }
+                
+			}
+            if (GeneralAttrib.metros % 75==0)
+            {
+                Traps.death = 0;
+            }
+            
+
+		}
+		catch (System.NullReferenceException e)
+		{
+
+		}
+	
+	}
+}
---
+++ MobileGame PullAway/Assets/Scripts/achivements.cs
@@ -0,0 +1,49 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class achivements : MonoBehaviour {
+
+    public GameObject PreFab, PreFab1, PreFab2, PreFab3, PreFab4;
+    public static GameObject nuevesito;
+    private static Vector3 posOr = new Vector3(0,-4f,-0.1f);
+    private static Quaternion rot = Quaternion.EulerAngles(0, 0, 0);
+    private static ArrayList prefabs = new ArrayList();
+
+	// Use this for initialization
+	void Start () 
+    {
+        prefabs.Add(PreFab);
+        prefabs.Add(PreFab1);
+        prefabs.Add(PreFab2);
+        prefabs.Add(PreFab3);
+        prefabs.Add(PreFab4);
+        InvokeRepeating("vachagnon", 0f, 1.5f);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        
+	
+	}
+
+    public void vachagnon()
+    {
+        if (GameObject.FindGameObjectWithTag("logro") != null)
+        {
+            Destroy(GameObject.FindGameObjectWithTag("logro"));
+        }
+    }
+
+    public static void Generar(int rnd)
+    {
+        rnd = Random.Range(1, 4);
+        ((GameObject)prefabs[rnd]).name = "logro";
+        nuevesito =(GameObject)Instantiate((GameObject)prefabs[rnd], posOr, rot);
+    }
+    public static void Generar()
+    {
+        ((GameObject)prefabs[0]).name = "logro";
+        nuevesito = (GameObject)Instantiate((GameObject)prefabs[0], posOr, rot);
+    }
+}
