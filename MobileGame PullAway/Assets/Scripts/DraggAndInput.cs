d913fdcf803c24afaab25ce27aae2851066fa59d
firefoxfenix <christiancarriespi@Gmail.com>
2016-03-30 19:20:54 -0500

    Cambios scripts

--- MobileGame PullAway/Assets/Scripts/DraggAndInput.cs
+++ MobileGame PullAway/Assets/Scripts/DraggAndInput.cs
@@ -2,245 +2,125 @@
 using System.Collections;
 
 public class DraggAndInput : MonoBehaviour {
-	
-	private Touch touch;
-	private Rigidbody2D Velocidad;
-	private Vector3 cambioV= new Vector3(), actual= new Vector3(), cambioH= new Vector3();
-	private string nombre;
-	private bool bd = false, bdT=false;
-	private Vector3 Pos;
-	private GameObject player, temp, enemy;
-	private SpriteRenderer spriteRenderer;
-	private RaycastHit colision;
-	private float DistanceY, DistanceX;
-	private AtribRocks atb;
-    private powerupsAtt powA;
-	private Vector3 Temp;
-    private TrapsAtt att;
-    private TrophiesAtt troph;
-	private static int contV=0;
 
-	// Use this for initialization
-	void Start () 
-	{
-		temp = GameObject.Find ("Temporito");
-	}
-	
-	
-	// Update is called once per frame
-	void Update () 
-	{
-		try
-		{
-			Dragging ();
-			Velocidad = player.GetComponent<Rigidbody2D> ();
-			spriteRenderer = player.GetComponent<SpriteRenderer>();
+    private Touch touch;
+    private Rigidbody2D RigidB;
+    private string nombre;
+    private bool bdT = false;
+    private Vector3 Pos;
+    private GameObject player, temp;
+    private SpriteRenderer spriteRenderer;
+    private AtribRocks atb;
+    private Vector3 Temp;
+    private static int contV = 0;
+
+    // Use this for initialization
+    void Start()
+    {
+        temp = GameObject.Find("Temporito");
+    }
 
 
-		}catch(System.NullReferenceException ex)		
-		{
-			player = temp;
-		}
-		catch (System.MissingFieldException ex)
-		{
+    // Update is called once per frame
+    void Update()
+    {
+        try
+        {
+            Dragging();
+            RigidB = player.GetComponent<Rigidbody2D>();
+            spriteRenderer = player.GetComponent<SpriteRenderer>();
+        }
+        catch (System.Exception e)
+        {
+            player = temp;
+        }
+    }
 
-		}
-		catch (System.Exception e)
-		{
+    public void Dragging()
+    {
 
-		}
-	}
-
-	public void Dragging()
-	{
-		
-		if (Input.touchCount > 0) 
-		{
-			if (Input.GetTouch (0).phase == TouchPhase.Began) 
-			{
-				//Cambia la posicion del touch por la posicion en la pantalla, es como un render
-				Pos = Camera.main.ScreenToWorldPoint (Input.GetTouch (0).position);
-				//Usamos un raycast 2d para hallar el collider
-				RaycastHit2D hit = Physics2D.Raycast (new Vector2 (Pos.x, Pos.y), Vector2.zero);
-				
-				if (hit != null && hit.collider != null) {
-					bd = true;
-					//designamos el objeto a mover dependiendo del collider tocado
-					player = GameObject.Find (hit.collider.gameObject.name);
-                    if (player.tag.CompareTo("Trampa1") == 0 || player.tag.CompareTo("Trampa2") == 0)
-                    {
-                        att = player.GetComponent<TrapsAtt>();
-                        contV++;
-                        perderVidas(contV,att);
-
-                    }
-                    else if (player.tag.CompareTo("Power") == 0)
-                    {
-                        powA = player.GetComponent<powerupsAtt>();
-                        contV++;
-                        perderVidas(contV,powA);
-                    }
-                    else if (player.tag.CompareTo("Trophie") == 0)
-                    {
-                        PlayerPrefs.SetInt("trop"+ player.GetComponent<TrophiesAtt>().tipo, 1);
-                        
-                        Destroy(player);
-                    }
-                    else 
-                    {
-                        if (GeneralAttrib.type == 1)
-                        {
-                            atb = player.GetComponent<AtribRocks>();
-                            contV++;
-                            perderVidas(contV);
-                        }
-                        
-                    }
-					
-				}
-				else
-				{
-					player = temp;
-				}
-			}
-            if (player.tag.CompareTo("roca") == 0)
+        if (Input.touchCount > 0)
+        {
+            if (Input.GetTouch(0).phase == TouchPhase.Began)
             {
-                Debug.Log(player.name+"12");
-                if (GeneralAttrib.type == 1)
-                {
-                    if ((player.gameObject.name != "Temporito") && (atb.life <= 0))
-                    {
-                        transform.FindChild("GameObject").GetComponent<Animator>().SetBool("life",true);
-                        Debug.Log("HEllo me animo");
-                        Pos = Camera.main.ScreenToWorldPoint(Input.GetTouch(0).position);
-                        BorrarLuego();
-                        if (bdT)
-                        {
-                            Velocidad.AddForce(new Vector2(DistanceX * 1f, DistanceY * 1f));
-
-                        }
-                        else
-                        {
-
-                            player.transform.position = new Vector2(Pos.x, Pos.y);
-
-                        }
-
-                    }
+                //Cambia la posicion del touch por la posicion en la pantalla, es como un render
+                Pos = Camera.main.ScreenToWorldPoint(Input.GetTouch(0).position);
+                //Usamos un raycast 2d para hallar el collider
+                RaycastHit2D hit = Physics2D.Raycast(new Vector2(Pos.x, Pos.y), Vector2.zero);
+                if (hit.collider != null) {
+                    bdT = true;
+                    //designamos el objeto a mover dependiendo del collider tocado
+                    player = GameObject.Find(hit.collider.gameObject.name);
                 }
                 else
                 {
-                    if (player.gameObject.name != "Temporito")
+                    player = temp;
+                }
+            }
+            if (player.tag.CompareTo("roca") == 0)
+            {
+                //Baja la vida de la roca
+                if (GeneralAttrib.type == 1)
+                {
+                    atb = player.GetComponent<AtribRocks>();
+                    contV++;
+                    perderVidas(contV);
+                }
+                if (atb.life <= 0)
+                {
+                    //Animacion
+                    transform.FindChild("GameObject").GetComponent<Animator>().SetBool("life", true);
+                    if (Input.GetTouch(0).phase == TouchPhase.Began)
                     {
-                        Pos = Camera.main.ScreenToWorldPoint(Input.GetTouch(0).position);
-                        BorrarLuego();
-                        Debug.Log("hi");
-                        if (bdT)
-                        {
-                            Debug.Log("hello");
-                            Velocidad.AddForce(new Vector2(DistanceX * 1f, DistanceY * 1f));
-
-                        }
-                        //else
-                        //{
-
-                        //    player.transform.position = new Vector2(Pos.x, Pos.y);
-
-                        //}
+                        bdT = false;
+                    }
+                    if (Input.GetTouch(0).phase == TouchPhase.Ended)
+                    {
+                        bdT = true;
+                    }
+                    if (bdT)
+                    {
+                        Debug.Log("1FuerzaX: " + Input.GetTouch(0).deltaPosition.x + " " + Input.GetTouch(0).deltaTime);
+                        Debug.Log("1FuerzaY: " + Input.GetTouch(0).deltaPosition.y + " " + Input.GetTouch(0).deltaTime);
+                        RigidB.AddForce(Vector2.Scale(Input.GetTouch(0).position, Input.GetTouch(0).deltaPosition) * Input.GetTouch(0).deltaTime);
 
                     }
+                    else
+                    {
+                        player.transform.position = new Vector2(Pos.x, Pos.y);
+                    }
+
                 }
-                
+
             }
-			
-			if (Input.GetTouch (0).phase == TouchPhase.Ended) 
-			{
-				contV=0;
-			}
 
-		}
+            if (Input.GetTouch(0).phase == TouchPhase.Ended)
+            {
+                contV = 0;
+            }
 
-	}
+        }
 
-	public void perderVidas(int cont)
-	{
-		if (cont == 1) 
-		{
-			atb.life-=1;
-			cont=5;
-		}
-	}
-    public void perderVidas(int cont, TrapsAtt at)
+    }
+
+    public void perderVidas(int cont)
     {
         if (cont == 1)
         {
-            at.life -= 1;
-            cont = 5;
-        }
-        if (at.life <= 0)
-        {
-            Destroy(player);
-        }
-    }
-    public void perderVidas(int cont, powerupsAtt at)
-    {
-        if (cont == 1)
-        {
-            at.powerVida -= 1;
+            atb.life -= 1;
             cont = 5;
         }
     }
+    
 
-	public void BorrarLuego()
-	{
-		if (Input.touchCount > 0) 
-		{
-			if (Input.GetTouch (0).phase == TouchPhase.Began) 
-			{
-				Temp = Input.GetTouch (0).position;
-				bdT=false;
-			}
+    /*
+        //Me regresara la posicion con 1 miliSegundo despues.
+        public void TemporalTouch()
+        {
+            if (Input.touchCount > 0)
+                Temp = Input.GetTouch (0).position;
+        }
 
-			foreach (Touch touch in Input.touches)
-			{
-				DistanceY = GetYAxisVelocity();
-				DistanceX = GetXAxisVelocity();
-				Temp = Input.GetTouch(0).position;
-			}
-			if(Input.GetTouch (0).phase == TouchPhase.Ended)
-			{
-				bdT=true;
-			}
-		}
-
-	}
-	public float GetYAxisVelocity()
-	{
-		if (Input.touchCount > 0) {
-			return  Input.GetTouch(0).position.y - Temp.y;
-		} else 
-		{
-			//Temp.y = 0;
-			return Velocidad.velocity.y;
-		}
-	}
-
-	public float GetXAxisVelocity()
-	{
-		if (Input.touchCount > 0) {
-			return   Input.GetTouch(0).position.x - Temp.x;
-		} else
-		{
-			//Temp.x = 0;
-			return Velocidad.velocity.x;
-		}
-	}
-/*
-	//Me regresara la posicion con 1 miliSegundo despues.
-	public void TemporalTouch()
-	{
-		if (Input.touchCount > 0)
-			Temp = Input.GetTouch (0).position;
-	}
-	*/
+        
+        */
 }
---
+++ MobileGame PullAway/Assets/Scripts/trampas/Create And Die.cs
@@ -0,0 +1,144 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Traps : MonoBehaviour {
+
+	// Use this for initialization
+	public GameObject prefabT1, prefabT2, prefabT3, prefabT4, prefabT5;
+	private int probT1,probT2;
+	private GameObject nuevo;
+    public static int maxAranas=0;
+    public static int maxBomba = 0;
+    public static int maxDerrumbes = 0;
+    public static int maxMinas = 0;
+    public static int maxFuego = 0;
+    public static int maxTrampas = 0;
+    private Vector3 Pos;
+    private GameObject player;
+    private powerupsAtt powA;
+    private TrapsAtt att;
+    private TrophiesAtt troph;
+
+    private Quaternion rotacion= Quaternion.Euler(0,0,0);
+
+	void Start () 
+	{
+        InvokeRepeating("prob", 0f, 5);
+	}
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.touchCount > 0)
+        {
+            if (Input.GetTouch(0).phase == TouchPhase.Began)
+            {
+                //Cambia la posicion del touch por la posicion en la pantalla, es como un render
+                Pos = Camera.main.ScreenToWorldPoint(Input.GetTouch(0).position);
+                //Usamos un raycast 2d para hallar el collider
+                RaycastHit2D hit = Physics2D.Raycast(new Vector2(Pos.x, Pos.y), Vector2.zero);
+                if(hit.collider!=null)
+                {
+                    if (player.tag.CompareTo("Trampa1") == 0)
+                    {
+                        att = player.GetComponent<TrapsAtt>();
+                        att.life--;
+
+                    }
+                    else if (player.tag.CompareTo("Power") == 0)
+                    {
+                        powA = player.GetComponent<powerupsAtt>();
+                        powA.powerVida--;
+                    }
+                    else if (player.tag.CompareTo("Trophie") == 0)
+                    {
+                        PlayerPrefs.SetInt("trop" + player.GetComponent<TrophiesAtt>().tipo, 1);
+                        Destroy(player);
+                    }
+
+                }
+            }
+        }
+    }
+
+
+    public void prob()
+    {
+        float proba = Random.Range(1, 100);
+        if (0 < maxTrampas && GameObject.FindGameObjectWithTag("Trampa2") == null && GameObject.FindGameObjectWithTag("Trampa1") == null)
+        {
+            crearT();
+        }
+    }
+    public void crearT()
+    {
+        probT1 = Random.Range(1, 100);
+        probT2 = Random.Range(1, 100);
+        Vector3 origen=new Vector3(Random.Range(-2,2), Random.Range(-4,4), 0);
+        if (GeneralAttrib.arcade)
+        {
+            if (0 < maxAranas && probT1<=20)
+            {
+                maxTrampas--;
+                maxAranas--;
+                nuevo = (GameObject)Instantiate(prefabT1, origen, rotacion);
+            }
+            else if (0 < maxFuego && probT1 <= 40)
+            {
+                maxTrampas--;
+                maxFuego--;
+                nuevo = (GameObject)Instantiate(prefabT2, origen, rotacion);
+            }
+            else if (0 < maxDerrumbes && probT1 <= 60)
+            {
+                maxTrampas--;
+                maxDerrumbes--;
+                nuevo = (GameObject)Instantiate(prefabT3, origen, rotacion);
+            }
+            else if (0 < maxMinas && probT1 <= 80)
+            {
+                maxTrampas--;
+                maxMinas--;
+                nuevo = (GameObject)Instantiate(prefabT4, origen, rotacion);
+            }
+            else if(0 < maxBomba)
+            {
+                maxTrampas--;
+                maxBomba--;
+                nuevo = (GameObject)Instantiate(prefabT5, origen, rotacion);
+            }
+        }
+        else
+        {
+            if (probT1 < (Map(GeneralAttrib.metros, 300, 100) * 5) + 25)
+            {
+                nuevo = (GameObject)Instantiate(prefabT1, origen, rotacion);
+            }
+            else if ((Map(GeneralAttrib.metros, 300, 100) < 3) && (probT2 < (Map(GeneralAttrib.metros, 300, 100) * 5) + 25))
+            {
+                nuevo = (GameObject)Instantiate(prefabT2, origen, rotacion);
+            }
+            else if ((Map(GeneralAttrib.metros, 300, 100) > 3) && (probT2 < (Map(GeneralAttrib.metros, 300, 100) * 5) + 15))
+            {
+                nuevo = (GameObject)Instantiate(prefabT2, origen, rotacion);
+            }
+        }
+    }
+
+	public int Map(double numIni, double oldMax, double oldMin)
+	{
+		if (numIni >= 2 * (oldMax - oldMin) / 3) 
+		{
+			return 3;
+		} 
+		else if ((numIni <= 2 * (oldMax - oldMin) / 3) && (numIni >= (oldMax - oldMin) / 3)) 
+		{
+			return 2;
+		} 
+		else 
+		{
+			return 1;
+		}
+	}
+
+}
